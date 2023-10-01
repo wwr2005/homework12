31d9f681a4ebef8b1eff11a9572ec6916e9f24c9
wwr2005 <wwr20005@gmail.com>
2023-10-01 21:46:07 +0300

    homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String... args) {
+        Author author1 = new Author("Гавриил","Троепо́льский");
+        Author author2 = new Author("Иван"," Тургенев");
+
+        Books houseworkWhiteBimBlackEar = new Books("Белый бим черное ухо", author1, 1971);
+        Books muMu = new Books("Му Му", author2, 1854);
+
+        System.out.println(
+               "Книга " + houseworkWhiteBimBlackEar.getTitle() + " " + "Автор: " + houseworkWhiteBimBlackEar.getAuthorFullName() + " " + "Год издания " + houseworkWhiteBimBlackEar.getPublicationYear());
+        System.out.println(" ");
+        System.out.println(
+                "Книга " + muMu.getTitle() + " " + "Автор: " + muMu.getAuthorFullName() + " " + "Год издания " + muMu.getPublicationYear());
     }
 }
